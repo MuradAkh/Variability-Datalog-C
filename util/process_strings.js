504d69a84968565553ebb553b3dfa4e69a00dc4e
Murad Akhundov <muradakhdev@gmail.com>
2020-06-06 19:21:40 -0400

    more regex preprocessing

--- util/process_strings.js
+++ util/process_strings.js
@@ -1,5 +1,13 @@
 const fs = require('fs'); 
 const file = fs.readFileSync('_temp/output.cfg', 'utf-8'); 
-const regex = /\".*\"/g;
-fs.writeFileSync('_temp/output.cfg', file.replace(regex, "\"\"")); 
+const regex = /\".*?\"/g;
+
+fs.writeFileSync(
+    '_temp/output.cfg', 
+    file
+    .replace(/\\\"/g, "")
+    .replace(regex, "\"\"")
+    .replace(/%/g, "_") 
+    .replace(/\'.*?\'/g, "\'A\'") 
+); 
 
